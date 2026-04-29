d0dad7a04a31b11bd86e4a2d8926b9c468998f4e
SrihariUdugula <srihari.u@olivecrypto.com>
2026-04-29 12:45:00 +0530

    initail setup

---
+++ src/main/java/com/hari/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.hari.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String practice() {
+		return "welcome to home page.., go head";
+	}
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
